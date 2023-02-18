8b748f4d73450b256e1cc4692fe596cdce4feec5
Martz <github@jmartz.de>
2023-02-18 19:30:26 +0100

    add logic for get One and get First, update phpDocs

--- src/Collection.php
+++ src/Collection.php
@@ -37,15 +37,14 @@ class Collection
      * @param array $queryParams
      * @return mixed
      */
-    public function getFullList(int $batch = 200, array $queryParams = [])
-    {
+    public function getFullList(int $batch = 200, array $queryParams = []){
         $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $this->url . "/api/collections/" . $this->collection . "/records");
+        curl_setopt($ch, CURLOPT_URL, $this->url . "/api/collections/".$this->collection."/records");
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        if (self::$token != '') {
+        if(self::$token != ''){
             $headers = array(
                 'Content-Type:application/json',
-                'Authorization: ' . self::$token
+                'Authorization: '.self::$token
             );
             curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
         }
@@ -60,22 +59,8 @@ class Collection
      * @param array $queryParams
      * @return void
      */
-    public function getFirstListItem(string $filter, array $queryParams = [])
-    {
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $this->url . "/api/collections/" . $this->collection . "/records?perPage=&page=1");
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        if (self::$token != '') {
-            $headers = array(
-                'Content-Type:application/json',
-                'Authorization: ' . self::$token
-            );
-            curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-        }
-        $output = curl_exec($ch);
-        curl_close($ch);
+    public function getFirstListItem(string $filter, array $queryParams = []){
 
-        return json_decode($output, JSON_FORCE_OBJECT);
     }
 
     /**
@@ -83,23 +68,23 @@ class Collection
      * @param array $queryParams
      * @return void
      */
-    public function create(array $bodyParams = [], array $queryParams = [])
-    {
+    public function  create(array $bodyParams = [], array $queryParams = []){
         $ch = curl_init();
 
-        if (self::$token != '') {
+        if(self::$token != ''){
             $headers = array(
                 'Content-Type:application/json',
-                'Authorization: ' . self::$token
+                'Authorization: '.self::$token
             );
             curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
         }
 
-        curl_setopt($ch, CURLOPT_URL, $this->url . "/api/collections/" . $this->collection . "/records");
+        curl_setopt($ch, CURLOPT_URL, $this->url . "/api/collections/".$this->collection."/records");
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
         curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($bodyParams));
         $output = curl_exec($ch);
+        var_dump($output);
         curl_close($ch);
     }
 
@@ -109,23 +94,23 @@ class Collection
      * @param array $queryParams
      * @return void
      */
-    public function update(string $recordId, array $bodyParams = [], array $queryParams = [])
-    {
+    public function  update(string $recordId, array $bodyParams = [], array $queryParams = []){
         $ch = curl_init();
 
-        if (self::$token != '') {
+        if(self::$token != ''){
             $headers = array(
                 'Content-Type:application/json',
-                'Authorization: ' . self::$token
+                'Authorization: '.self::$token
             );
             curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
         }
 
-        curl_setopt($ch, CURLOPT_URL, $this->url . "/api/collections/" . $this->collection . "/records/" . $recordId);
+        curl_setopt($ch, CURLOPT_URL, $this->url . "/api/collections/".$this->collection."/records/".$recordId);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'PATCH');
         curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($bodyParams));
         $output = curl_exec($ch);
+        var_dump($output);
         curl_close($ch);
     }
 
@@ -134,19 +119,18 @@ class Collection
      * @param array $queryParams
      * @return void
      */
-    public function delete(string $recordId, array $queryParams = []):void
-    {
+    public function delete(string $recordId, array $queryParams = []){
         $ch = curl_init();
 
-        if (self::$token != '') {
+        if(self::$token != ''){
             $headers = array(
                 'Content-Type:application/json',
-                'Authorization: ' . self::$token
+                'Authorization: '.self::$token
             );
             curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
         }
 
-        curl_setopt($ch, CURLOPT_URL, $this->url . "/api/collections/" . $this->collection . "/records/" . $recordId);
+        curl_setopt($ch, CURLOPT_URL, $this->url . "/api/collections/".$this->collection."/records/".$recordId);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'DELETE');
         $output = curl_exec($ch);
@@ -156,11 +140,26 @@ class Collection
     /**
      * @param string $recordId
      * @param array $queryParams
-     * @return void
+     * @return mixed
      */
-    public function getOne(string $recordId, array $queryParams = [])
-    {
-        // Todo: implibment logic
+    public function getOne(string $recordId, array $queryParams = []){
+        $ch = curl_init();
+
+        if(self::$token != ''){
+            $headers = array(
+                'Content-Type:application/json',
+                'Authorization: '.self::$token
+            );
+            curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        }
+
+        curl_setopt($ch, CURLOPT_URL, $this->url . "/api/collections/".$this->collection."/records/".$recordId);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
+        $output = curl_exec($ch);
+        curl_close($ch);
+
+        return json_decode($output, JSON_FORCE_OBJECT);
     }
 
     /**
